a3ffa4058436b144469b89f84512018990520328
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-04 15:35:46 +0300

    Added Generic Repository

---
+++ Project.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Core.Models;
+
+namespace Project.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> exp);
+        IQueryable<T> Where(Expression<Func<T,bool>> exp);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> exp);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+
+    }
+}
