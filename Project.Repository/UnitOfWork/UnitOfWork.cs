52e448a5db653c498c71b02e00af34bc0928d022
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-04 21:49:28 +0300

    Added concrete unit of work pattern

---
+++ Project.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Project.Core.UnitOfWorks;
+using Project.Repository.Context;
+
+namespace Project.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        private readonly YurtDbContext _context;
+
+        public UnitOfWork(YurtDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
