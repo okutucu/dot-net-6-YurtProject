7eab2bfa16415d85edfb727b3af6fea9d2ba9561
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-04 20:23:03 +0300

    Added Assembly GetExecutingAssembly

--- Project.Repository/Context/YurtDbContext.cs
+++ Project.Repository/Context/YurtDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,13 @@ namespace Project.Repository.Context
         public DbSet<Room> Rooms { get; set; }
         public DbSet<RoomIncome> RoomIncomes { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
 
     }
 }
