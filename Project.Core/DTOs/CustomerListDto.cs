ae3ee759ee8b145707c701dc52d250739b832b93
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-05 12:02:53 +0300

    Added DTOs

---
+++ Project.Core/DTOs/CustomerListDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Project.Core.DTOs
+{
+    public class CustomerListDto : BaseDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime Depart { get; set; }
+        public string RelativeNameSurname { get; set; }
+        public string RelativePhone { get; set; }
+        public string UniversityName { get; set; }
+        public string Description { get; set; }
+    }
+}
