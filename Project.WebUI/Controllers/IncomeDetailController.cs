2bee132e56035c2e36a3b84b6a4694b04c69fa79
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-23 00:57:51 +0300

    ++

---
+++ Project.WebUI/Controllers/IncomeDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.WebUI.Controllers
+{
+	public class IncomeDetailController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
