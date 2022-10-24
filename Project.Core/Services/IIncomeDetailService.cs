8f74a5929b282f1e9dcb0d1736798281af31fd2d
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-24 12:41:51 +0300

    completed incomedetail  service

--- Project.Core/Services/IIncomeDetailService.cs
+++ Project.Core/Services/IIncomeDetailService.cs
@@ -11,6 +11,11 @@ namespace Project.Core.Services
 	public interface IIncomeDetailService : IService<IncomeDetail>
 	{
         Task<List<IncomeDetailDto>> GetIncomeWithRoomAsync();
+        Task AddByCurrency(IncomeDetailDto incomeDetailDto, decimal currency);
+        Task UpdateByCurrency(IncomeDetailDto incomeDetailDto, decimal currency);
+        Task<List<IncomeDetailDto>> DailyOrMonthly(string selectedDate);
+        Task<List<IncomeDetailDto>> GetByMonth(int year, int month);
+        Task<List<IncomeDetailDto>> GetByDay(int year, int month, int day);
 
     }
 }
--- Project.Service/Services/IncomeDetailService.cs
+++ Project.Service/Services/IncomeDetailService.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Project.Core.DTOs;
 using Project.Core.Models;
 using Project.Core.Repositories;
 using Project.Core.Services;
 using Project.Core.UnitOfWorks;
-using Project.Repository.Repositories;
 
 namespace Project.Service.Services
 {
-	public class IncomeDetailService : Service<IncomeDetail>, IIncomeDetailService
+    public class IncomeDetailService : Service<IncomeDetail>, IIncomeDetailService
 	{
 		private readonly IIncomeDetailRepository _incomeDetailRepository;
         private readonly IMapper _mapper;
@@ -25,6 +19,63 @@ namespace Project.Service.Services
             _mapper = mapper;
         }
 
+		public async Task AddByCurrency(IncomeDetailDto incomeDetailDto, decimal currency)
+		{
+            incomeDetailDto.MoneyOfTheDay = currency * incomeDetailDto.Price;
+            incomeDetailDto.PaymentDate = DateTime.Now;
+
+
+            await _incomeDetailRepository.AddAsync(_mapper.Map<IncomeDetail>(incomeDetailDto));
+            await _unitOfWok.CommitAsync();
+        }
+
+		public async Task<List<IncomeDetailDto>> DailyOrMonthly(string selectedDate)
+		{
+            if (selectedDate != null)
+            {
+                int month;
+                int year;
+                string[] subs = selectedDate.Split('-');
+
+                if (subs.Length == 2)
+                {
+                    year = int.Parse(subs[0]);
+                    month = int.Parse(subs[1]);
+
+                    return await GetByMonth(year, month);
+                }
+                else if (subs.Length == 3)
+                {
+                    year = int.Parse(subs[0]);
+                    month = int.Parse(subs[1]);
+                    int day = int.Parse(subs[2].Substring(0, 2));
+
+                    return await GetByDay(year, month, day);
+
+                }
+            }
+
+            throw new Exception("An error occurred in the date format");
+        }
+
+		public async Task<List<IncomeDetailDto>> GetByDay(int year, int month, int day)
+		{
+            List<IncomeDetail> incomeDetails = _incomeDetailRepository.Where(p => p.PaymentDate.Year == year && p.PaymentDate.Month == month && p.PaymentDate.Day == day).ToList();
+
+            List<IncomeDetailDto> incomeDetailDto = _mapper.Map<List<IncomeDetailDto>>(incomeDetails);
+
+            return incomeDetailDto;
+        }
+
+		public async Task<List<IncomeDetailDto>> GetByMonth(int year, int month)
+		{
+            List<IncomeDetail> incomeDetails = _incomeDetailRepository.Where(p => p.PaymentDate.Year == year && p.PaymentDate.Month == month).ToList();
+
+            List<IncomeDetailDto> incomeDetailDto = _mapper.Map<List<IncomeDetailDto>>(incomeDetails);
+
+            return incomeDetailDto;
+        }
+
 		public async Task<List<IncomeDetailDto>> GetIncomeWithRoomAsync()
 		{
 			List<IncomeDetail> incomeDetails = await _incomeDetailRepository.GetIncomeWithRoomAsync();
@@ -33,6 +84,16 @@ namespace Project.Service.Services
 
 			return incomeDetailsDto;
 
+        }
+
+		public async Task UpdateByCurrency(IncomeDetailDto incomeDetailDto, decimal currency)
+		{
+            incomeDetailDto.MoneyOfTheDay = currency * incomeDetailDto.Price;
+            incomeDetailDto.PaymentDate = DateTime.Now;
+
+
+            _incomeDetailRepository.Update(_mapper.Map<IncomeDetail>(incomeDetailDto));
+            await _unitOfWok.CommitAsync();
         }
 	}
 }
