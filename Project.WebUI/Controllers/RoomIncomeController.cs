091265400b1c22fb3410fc302b9cb7c0866fbdad
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-24 16:10:38 +0300

    Added Room Income Controllers and index view

---
+++ Project.WebUI/Controllers/RoomIncomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.WebUI.Controllers
+{
+	public class RoomIncomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
