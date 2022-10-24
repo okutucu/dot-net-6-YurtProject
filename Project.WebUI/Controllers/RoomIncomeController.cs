f7e1e88f5b5e80a7a68e50bb3ab33b3c3ec39def
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-24 17:52:16 +0300

    ++

--- Project.WebUI/Controllers/RoomIncomeController.cs
+++ Project.WebUI/Controllers/RoomIncomeController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Project.Core.DTOs;
+using Project.Core.Models;
+using Project.Core.Services;
 
 namespace Project.WebUI.Controllers
 {
 	public class RoomIncomeController : Controller
 	{
-		public IActionResult Index()
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+
+
+        public RoomIncomeController(IRoomService roomService, IMapper mapper)
+		{
+			_roomService = roomService;
+			_mapper = mapper;
+		}
+
+        public IActionResult Index()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		public async Task<JsonResult> GetByDept(int id)
+		{
+			Room room = await _roomService.GetByIdAsync(id);
+
+			JsonResult result = Json(room.Debt);
+			
+            return result;
+        }
+
+		public IActionResult Create()
+		{
+            List<Room> rooms = _roomService.GetAll().ToList();
+
+            List<RoomDto> roomsDto = _mapper.Map<List<RoomDto>>(rooms);
+
+            ViewBag.rooms = new SelectList(roomsDto, "Id", "RoomName");
+
+            return View();
 		}
 	}
 }
