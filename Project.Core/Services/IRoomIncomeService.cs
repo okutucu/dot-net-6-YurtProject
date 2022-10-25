ae5cdb0badddd225988cac6813b011eaa059f854
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-25 15:54:05 +0300

    Created RoomIncome repository and services

---
+++ Project.Core/Services/IRoomIncomeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Core.DTOs;
+using Project.Core.Models;
+
+namespace Project.Core.Services
+{
+    public interface IRoomIncomeService : IService<RoomIncome>
+    {
+        Task AddByCurrency(RoomIncomeDto roomIncomeDto, decimal currency);
+        Task UpdateByCurrency(RoomIncomeDto roomIncomeDto, decimal currency);
+    }
+}
---
+++ Project.Service/Services/RoomIncomeService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Core.DTOs;
+using Project.Core.Models;
+using Project.Core.Repositories;
+using Project.Core.Services;
+using Project.Core.UnitOfWorks;
+
+namespace Project.Service.Services
+{
+    public class RoomIncomeService : Service<RoomIncome>, IRoomIncomeService
+    {
+        public RoomIncomeService(IUnitOfWork unitOfWok, IGenericRepository<RoomIncome> repository) : base(unitOfWok, repository)
+        {
+        }
+
+        public Task AddByCurrency(RoomIncomeDto roomIncomeDto, decimal currency)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateByCurrency(RoomIncomeDto roomIncomeDto, decimal currency)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
