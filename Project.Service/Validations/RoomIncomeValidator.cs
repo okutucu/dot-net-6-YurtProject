96fc8a29add8f96bd570853749fa6f08f0c2988d
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-10-29 14:52:06 +0300

    completed RoomIncomeValidator and other operations

--- Project.Service/Validations/RoomIncomeValidator.cs
+++ Project.Service/Validations/RoomIncomeValidator.cs
@@ -13,6 +13,8 @@ namespace Project.Service.Validations
     {
         public RoomIncomeValidator()
         {
+
+            //todo control validaton
             RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater then 0");
             RuleFor(x => x.Exchange).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("(PropertyName) is required");
             RuleFor(x => x.PaymentMethod).NotNull().WithMessage("{PropertyName} is required").NotEmpty().WithMessage("(PropertyName) is required");
