431389388ff0822f6649670c5005063c807b1e05
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-11-02 15:31:38 +0200

    added SendAMailToAllUsers

--- Project.Service/Validations/RoomIncomeValidator.cs
+++ Project.Service/Validations/RoomIncomeValidator.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentValidation;
+﻿using FluentValidation;
 using Project.Core.DTOs;
-using Project.Core.Models;
 
 namespace Project.Service.Validations
 {
